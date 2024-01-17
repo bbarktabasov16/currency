95cef078e3b927c4fffabd5ef4c665caef884668
Bekzat <118148584+bbarktabasov16@users.noreply.github.com>
2024-01-17 16:00:26 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let transSumEl = document.getElementById('currency')
+let mountEl = document.getElementById('mount')
+let sumEl = document.getElementById('sum')
+let resultEl = document.getElementById('result')
+
+const sumCurrent = () => {
+    let transEl = transSumEl.value
+    let mount = mountEl.value
+
+    let resEl = transEl * mount
+
+    resultEl.innerHTML = resEl.toFixed(2)
+}
